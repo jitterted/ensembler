58a21608e8d4390cf9a1d1eb3ac9b3d71eeb9625
Ted M. Young <tedyoung@gmail.com>
2021-06-11 15:32:58 -0700

    Added new home page using a bit of Tailwind CSS.

--- src/main/java/com/jitterted/mobreg/WebSecurityConfig.java
+++ src/main/java/com/jitterted/mobreg/WebSecurityConfig.java
@@ -26,8 +26,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
                 .mvcMatchers("/", "/error")
                     .permitAll()
-                .mvcMatchers("/user/**")
-                    .hasAuthority("ROLE_USER")
+//                .mvcMatchers("/user/**")
+//                    .hasAuthority("ROLE_USER") // TODO: what?
                 .mvcMatchers("/admin/**")
                     .hasAuthority("ROLE_ADMIN")
                 .mvcMatchers("/member/**")
