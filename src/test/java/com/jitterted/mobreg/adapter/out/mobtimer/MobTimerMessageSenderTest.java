097cc4a7f7823e80d0601909dd3e667498800edc
Ted M. Young <tedyoung@gmail.com>
2021-08-12 17:42:32 -0700

    Updated Mob Timer to work with the new registration mechanism.

--- src/test/java/com/jitterted/mobreg/adapter/out/mobtimer/MobTimerMessageSenderTest.java
+++ src/test/java/com/jitterted/mobreg/adapter/out/mobtimer/MobTimerMessageSenderTest.java
@@ -1,12 +1,15 @@
 package com.jitterted.mobreg.adapter.out.mobtimer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jitterted.mobreg.adapter.out.membership.GitHubCollaboratorMembershipValidator;
 import com.jitterted.mobreg.domain.Huddle;
 import com.jitterted.mobreg.domain.Member;
+import com.jitterted.mobreg.domain.MemberService;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.ZonedDateTime;
 
@@ -20,10 +23,18 @@ class MobTimerMessageSenderTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    GitHubCollaboratorMembershipValidator gitHubCollaboratorMembershipValidator;
+
+    @Autowired
+    MemberService memberService;
+
     @Test
     public void mobPeopleUpdated() throws Exception {
         Huddle huddle = new Huddle("test", ZonedDateTime.now());
-        huddle.register(new Member("WooHoo", ""));
+        Member member = new Member("WooHoo", "");
+        Member savedMember = memberService.save(member);
+        huddle.registerById(savedMember.getId());
 
         mobTimerMessageSender.updateParticipantsTo(huddle);
     }
