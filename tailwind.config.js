e6e1950675971bc92863628aa5e6e30184328870
Ted M. Young <tedyoung@gmail.com>
2021-09-14 19:39:59 -0700

    Modernized the member registration page using similar Tailwind CSS layout as the Admin dashboard.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,18 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
+        minWidth: {
+            '0': '0',
+            '1/4': '25%',
+            '1/2': '50%',
+            '3/4': '75%',
+            '4/5': '80%',
+            'full': '100%',
+        },
     },
     variants: {
         extend: {},
+        backgroundColor: ['responsive', 'first', 'last', 'even', 'odd', 'hover', 'focus'],
     },
     plugins: [
         require('@tailwindcss/forms'),
