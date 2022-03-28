03a170355c00a3c1abf71b2653912476b8acc00c
Lada Kesseler <archie.kesseler@gmail.com>
2022-03-28 12:31:19 -0700

    Cleaned up MemberFactory, got rid of unused methods and fields.

--- src/test/java/com/jitterted/mobreg/application/MemberFactory.java
+++ src/test/java/com/jitterted/mobreg/application/MemberFactory.java
@@ -1,22 +1,10 @@
 package com.jitterted.mobreg.application;
 
-import com.jitterted.mobreg.application.port.InMemoryMemberRepository;
-import com.jitterted.mobreg.application.port.MemberRepository;
-import com.jitterted.mobreg.domain.Ensemble;
 import com.jitterted.mobreg.domain.Member;
 import com.jitterted.mobreg.domain.MemberId;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class MemberFactory {
-
-    private final MemberRepository memberRepository;
-
-    public MemberFactory() {
-        this.memberRepository = new InMemoryMemberRepository();
-    }
 
     @NotNull
     public static Member createMember(long id, String firstName, String githubUsername) {
@@ -29,38 +17,5 @@ public class MemberFactory {
         Member member = new Member(firstName, githubUsername, roles);
         member.setId(MemberId.of(id));
         return member;
-    }
-
-    public static void acceptCountMembersWith(Ensemble ensemble, int count) {
-        MemberFactory memberFactory = new MemberFactory();
-        for (int i = 0; i < count; i++) {
-            MemberId memberId = memberFactory.createMemberInRepositoryReturningId(
-                i, "name" + i, "github" + i);
-            ensemble.acceptedBy(memberId);
-        }
-    }
-
-    public static List<Member> createAcceptedMembersFrom(Ensemble ensemble) {
-        return ensemble.acceptedMembers()
-            .map(memberId -> createMember(
-                memberId.id(),
-                "name" + memberId.id(),
-                "github" + memberId.id())
-            )
-            .collect(Collectors.toList());
-    }
-
-    @NotNull
-    public MemberId createMemberInRepositoryReturningId(long id, String firstName, String githubUsername) {
-        Member member = createMember(id, firstName, githubUsername);
-        memberRepository.save(member);
-        return member.getId();
-    }
-
-    public MemberId createMemberInRepositoryReturningId(long id, String firstName, String githubUsername, String email) {
-        Member member = createMember(id, firstName, githubUsername);
-        member.changeEmailTo(email);
-        memberRepository.save(member);
-        return member.getId();
     }
 }
