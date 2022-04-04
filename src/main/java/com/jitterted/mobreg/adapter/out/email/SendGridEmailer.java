ed1c7d551920c949164270f8f547ea3d824f6585
Ted M. Young <tedyoung@gmail.com>
2022-04-04 16:25:40 -0700

    Dump email prefixes instead of the Personalization object (which doesn't have a toString)

--- src/main/java/com/jitterted/mobreg/adapter/out/email/SendGridEmailer.java
+++ src/main/java/com/jitterted/mobreg/adapter/out/email/SendGridEmailer.java
@@ -8,12 +8,14 @@ import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
 import com.sendgrid.helpers.mail.objects.Personalization;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @Component
 public class SendGridEmailer implements Emailer {
@@ -43,9 +45,18 @@ public class SendGridEmailer implements Emailer {
         try {
             request.setBody(mail.build());
             Response response = sg.api(request);
-            LOGGER.info("Emails sent via SendGrid, response={}, personalization={}", response.getStatusCode(), personalization.getTos());
+            LOGGER.info("Emails sent via SendGrid, response={}, to emails={}", response.getStatusCode(), toEmailsAsStrings(personalization));
         } catch (IOException e) {
             LOGGER.warn("Exception when trying to send email via SendGrid. Request={}. Exception: {}", request.getBody(), e);
         }
+    }
+
+    @NotNull
+    private String toEmailsAsStrings(Personalization personalization) {
+        return personalization.getTos()
+                              .stream()
+                              .map(Email::getEmail)
+                              .map(email -> email.split("@")[0])
+                              .collect(Collectors.joining(","));
     }
 }
