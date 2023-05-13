6b316392d7235c7a7035b5de6a4bcec89b8ef242
Ted M. Young <tedyoung@gmail.com>
2023-05-13 13:52:40 -0700

    Add a bit of validation to show errors on the admin member management page.
    Rename test configurations to new terminology

--- src/main/java/com/jitterted/mobreg/adapter/in/web/admin/MemberManagementController.java
+++ src/main/java/com/jitterted/mobreg/adapter/in/web/admin/MemberManagementController.java
@@ -4,9 +4,11 @@ import com.jitterted.mobreg.application.port.MemberRepository;
 import com.jitterted.mobreg.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Comparator;
 import java.util.List;
 
@@ -21,22 +23,32 @@ public class MemberManagementController {
 
     @GetMapping("/admin/members")
     public String membersView(Model model) {
-        List<MemberView> memberViews = memberRepository.findAll()
-                                                       .stream()
-                                                       .sorted(Comparator.comparingLong(member -> member.getId().id()))
-                                                       .map(MemberView::from)
-                                                       .toList();
-        model.addAttribute("members", memberViews)
-             .addAttribute("addMemberForm", new AddMemberForm());
+        addAllMembersToModel(model);
+        model.addAttribute("addMemberForm", new AddMemberForm());
         return "members";
     }
 
     @PostMapping("/admin/add-member")
-    public String addMember(AddMemberForm addMemberForm) {
+    public String addMember(@Valid AddMemberForm addMemberForm,
+                            BindingResult bindingResult,
+                            Model model) {
+        if (bindingResult.hasErrors()) {
+            addAllMembersToModel(model);
+            return "members";
+        }
         Member member = new Member(addMemberForm.getFirstName(),
                                    addMemberForm.getGithubUsername(),
                                    "ROLE_USER", "ROLE_MEMBER");
         memberRepository.save(member);
         return "redirect:/admin/members";
+    }
+
+    private void addAllMembersToModel(Model model) {
+        List<MemberView> memberViews = memberRepository.findAll()
+                                                       .stream()
+                                                       .sorted(Comparator.comparingLong(member -> member.getId().id()))
+                                                       .map(MemberView::from)
+                                                       .toList();
+        model.addAttribute("members", memberViews);
     }
 }
