cc185a8f3580738fe32d1990a030bc44131a1ebf
Ted M. Young <tedyoung@gmail.com>
2024-03-26 14:01:20 -0700

    Expanded members for timer creation so there are 5 members.

--- src/test/java/com/jitterted/mobreg/domain/EnsembleTimerFactory.java
+++ src/test/java/com/jitterted/mobreg/domain/EnsembleTimerFactory.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class EnsembleTimerFactory {
     public static final EnsembleId IRRELEVANT_ENSEMBLE_ID = EnsembleId.of(53);
     public static final String IRRELEVANT_NAME = "Test";
+    private static final String IRRELEVANT_GITHUB_USERNAME = "irrelevant";
 
     public static Fixture create4MinuteTimerInFinishedState() {
         EnsembleTimer ensembleTimer = createTimerWith4MinuteDuration();
@@ -21,20 +22,22 @@ public class EnsembleTimerFactory {
     }
 
     public static EnsembleTimer createTimerWith4MinuteDurationAndIdOf(EnsembleId ensembleId) {
-        Member member1 = MemberFactory.createMember(1, "One", "irrelevant");
-        Member member2 = MemberFactory.createMember(2, "Two", "irrelevant");
-        Member member3 = MemberFactory.createMember(3, "Three", "irrelevant");
+        Member member1 = MemberFactory.createMember(1, "One", IRRELEVANT_GITHUB_USERNAME);
+        Member member2 = MemberFactory.createMember(2, "Two", IRRELEVANT_GITHUB_USERNAME);
+        Member member3 = MemberFactory.createMember(3, "Three", IRRELEVANT_GITHUB_USERNAME);
+        Member member4 = MemberFactory.createMember(4, "Four", IRRELEVANT_GITHUB_USERNAME);
+        Member member5 = MemberFactory.createMember(5, "Five", IRRELEVANT_GITHUB_USERNAME);
 
         return new EnsembleTimer(ensembleId,
                                  IRRELEVANT_NAME,
-                                 List.of(member1, member2, member3),
+                                 List.of(member1, member2, member3, member4, member5),
                                  Duration.ofMinutes(4));
     }
 
     public static EnsembleTimer createTimer() {
-        Member member1 = MemberFactory.createMember(1, "One", "irrelevant");
-        Member member2 = MemberFactory.createMember(2, "Two", "irrelevant");
-        Member member3 = MemberFactory.createMember(3, "Three", "irrelevant");
+        Member member1 = MemberFactory.createMember(1, "One", IRRELEVANT_GITHUB_USERNAME);
+        Member member2 = MemberFactory.createMember(2, "Two", IRRELEVANT_GITHUB_USERNAME);
+        Member member3 = MemberFactory.createMember(3, "Three", IRRELEVANT_GITHUB_USERNAME);
 
         return new EnsembleTimer(IRRELEVANT_ENSEMBLE_ID,
                                  IRRELEVANT_NAME,
