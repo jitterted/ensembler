332e6605a4077dd0133cbd024fd1c69ba3fd74b3
Ted M. Young <tedyoung@gmail.com>
2024-11-08 08:31:41 -0800

    Catch any exceptions thrown by individual broadcasters and log them

--- src/main/java/com/jitterted/mobreg/adapter/out/CompositeBroadcaster.java
+++ src/main/java/com/jitterted/mobreg/adapter/out/CompositeBroadcaster.java
@@ -2,11 +2,14 @@ package com.jitterted.mobreg.adapter.out;
 
 import com.jitterted.mobreg.application.port.Broadcaster;
 import com.jitterted.mobreg.domain.EnsembleTimer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class CompositeBroadcaster implements Broadcaster {
 
+    private static final Logger logger = LoggerFactory.getLogger(CompositeBroadcaster.class);
     private final List<Broadcaster> broadcasters;
 
     public CompositeBroadcaster(List<Broadcaster> broadcasters) {
@@ -20,6 +23,12 @@ public class CompositeBroadcaster implements Broadcaster {
 
     @Override
     public void sendEvent(EnsembleTimer.TimerEvent timerEvent) {
-        broadcasters.forEach(broadcaster -> broadcaster.sendEvent(timerEvent));
+        broadcasters.forEach(broadcaster -> {
+            try {
+                broadcaster.sendEvent(timerEvent);
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
+        });
     }
 }
