ab787860146db07c7b0d9e78aa4bd7951fec76df
Ted M. Young <tedyoung@gmail.com>
2024-11-21 12:59:04 -0800

    Upgraded to Spring Boot 3.4.0
    Disabled the automatic creation of an Ensemble with 5 Members—needs a more comprehensive fix, only needed when running locally.

--- src/main/java/com/jitterted/mobreg/EnsemblerConfiguration.java
+++ src/main/java/com/jitterted/mobreg/EnsemblerConfiguration.java
@@ -6,6 +6,7 @@ import com.jitterted.mobreg.adapter.out.jdbc.EnsembleJdbcRepository;
 import com.jitterted.mobreg.application.DefaultMemberService;
 import com.jitterted.mobreg.application.EnsembleService;
 import com.jitterted.mobreg.application.EnsembleTimerHolder;
+import com.jitterted.mobreg.application.MemberNotFoundByIdException;
 import com.jitterted.mobreg.application.MemberService;
 import com.jitterted.mobreg.application.port.Broadcaster;
 import com.jitterted.mobreg.application.port.EnsembleRepository;
@@ -19,7 +20,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import java.net.URI;
 import java.time.ZoneOffset;
@@ -63,21 +63,45 @@ public class EnsemblerConfiguration {
     // TODO: remove this once member registration works
     @Bean
     public CommandLineRunner commandLineRunner(MemberService memberService, MemberRepository memberRepository) {
-        return args -> {
+        return _ -> {
             if (memberRepository.findByGithubUsername("tedyoung").isEmpty()) {
                 memberService.save(new Member("Ted", "tedyoung", "ROLE_USER", "ROLE_MEMBER", "ROLE_ADMIN"));
             }
         };
     }
 
-    @Bean
-    @Profile("local")
-    public CommandLineRunner createInProgressEnsemble(EnsembleService ensembleService,
+//    @Bean
+//    @Profile("local")
+    public CommandLineRunner createInProgressEnsemble(MemberService memberService,
+                                                      MemberRepository memberRepository,
+                                                      EnsembleService ensembleService,
                                                       EnsembleJdbcRepository jdbcRepository) {
-        return args -> {
+        return _ -> {
             if (jdbcRepository.countAllByNameContainingIgnoreCase("Ensemble Timer Demo") > 2) {
                 System.err.println("Too many timer demo ensembles, deleting them.");
                 jdbcRepository.deleteWhereNameEquals("Ensemble Timer Demo");
+            }
+
+            try {
+                for (int i = 1; i <= 5; i++) {
+                    memberService.findById(MemberId.of(i));
+                }
+                System.out.println("Found all members with IDs 1 through 5, skipping member creation...");
+            } catch (MemberNotFoundByIdException e) {
+                System.out.println("Could not find all 5 test members, creating 5 new members...");
+                // wasn't found, so assume that no default members exist: let's go create them
+                for (int i = 1; i <= 5; i++) {
+                    if (memberRepository.findById(MemberId.of(i)).isEmpty()) {
+                        Member member = new Member("Member " + i, "member" + i, "ROLE_USER", "ROLE_MEMBER");
+                        member.setId(MemberId.of(i));
+                        try {
+                            memberService.save(member);
+                        } catch (Exception ex) {
+                            System.err.println("Could not save member: " + member);
+                            ex.printStackTrace();
+                        }
+                    }
+                }
             }
             Ensemble ensemble = ensembleService.scheduleEnsemble("Ensemble Timer Demo",
                                                                  URI.create("https://zoom.us"),
