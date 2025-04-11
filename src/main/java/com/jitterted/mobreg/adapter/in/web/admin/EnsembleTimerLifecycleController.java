2a4ba945b736bbc7f6a3939d37731de038e9cb2c
Ted M. Young <tedyoung@gmail.com>
2025-04-11 12:48:14 -0700

    Fixed vertical spacing

--- src/main/java/com/jitterted/mobreg/adapter/in/web/admin/EnsembleTimerLifecycleController.java
+++ src/main/java/com/jitterted/mobreg/adapter/in/web/admin/EnsembleTimerLifecycleController.java
@@ -71,7 +71,7 @@ public class EnsembleTimerLifecycleController {
         }
         return
                 """
-                <div class="flex flex-col space-y-2">
+                <div class="flex flex-col space-y-4">
                     <form action="/admin/create-timer/%s" method="post">
                         <button class="inline-flex items-center rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                         >
--- src/test/java/com/jitterted/mobreg/adapter/in/web/admin/EnsembleTimerLifecycleControllerTest.java
+++ src/test/java/com/jitterted/mobreg/adapter/in/web/admin/EnsembleTimerLifecycleControllerTest.java
@@ -78,7 +78,7 @@ class EnsembleTimerLifecycleControllerTest {
                                       <p>No timer currently exists for this Ensemble.</p>
                                   </swap>
                                   <swap id="timer-button-container" hx-swap-oob="innerHTML">
-                                      <div class="flex flex-col space-y-2">
+                                      <div class="flex flex-col space-y-4">
                                           <form action="/admin/create-timer/109" method="post">
                                               <button class="inline-flex items-center rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                                               >
@@ -167,7 +167,7 @@ class EnsembleTimerLifecycleControllerTest {
                                       </p>
                                   </swap>
                                   <swap id="timer-button-container" hx-swap-oob="innerHTML">
-                                      <div class="flex flex-col space-y-2">
+                                      <div class="flex flex-col space-y-4">
                                           <form action="/admin/create-timer/581" method="post">
                                               <button class="inline-flex items-center rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                                               >
